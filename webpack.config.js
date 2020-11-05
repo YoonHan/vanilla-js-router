8e8a4500ce322173bf5341705bd7a05eab3e91a5
Yoon Han <hyku2014@gmail.com>
2020-11-05 13:20:15 +0900

    Edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)|(dist)/,
+            loader: 'babel-loader'
+        }]
     }
 }
